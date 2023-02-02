68f597f2d22ca6d92161198440066d9a58f497da
Jay Jackson <jj@jj.is-a.dev>
2023-02-01 23:16:50 -0500

    fix up popup

--- popup.js
+++ popup.js
@@ -1,43 +1,44 @@
-chrome.storage.local.get("refocused_target_url").then(async (res) => {
-    //  tried to make this work with await but it didn't work
-    let targetUrl = res["refocused_target_url"];
-    let endCookies = [];
-    endCookies.push(
-        await chrome.cookies.get({
-            url: targetUrl,
-            name: "focus_token"
-        })
-    );
-    endCookies.push(
-        await chrome.cookies.get({
-            url: targetUrl,
-            name: "focus_session_id"
-        })
-    );
-    endCookies.push(
-        await chrome.cookies.get({
-            url: targetUrl,
-            name: "focus_php_session_id"
-        })
-    );
-    console.log(endCookies);
+(async () => {
+    const url = chrome.storage.local.get("targetUrl");
 
-    if (endCookies.every((cookie) => cookie !== null)) {
-        document.getElementById("description").innerHTML =
-            "Existing Auth Cookies found: <br> " +
-            endCookies
+    /**
+     * @type {{name: string, value: string}[]}
+     */
+    let cookies = [
+        await chrome.cookies.get({
+            url,
+            name: "focus_token"
+        }),
+        await chrome.cookies.get({
+            url,
+            name: "focus_session_id"
+        }),
+        await chrome.cookies.get({
+            url,
+            name: "focus_php_session_id"
+        }),
+        await chrome.cookies.get({
+            url,
+            name: "focus_login_token"
+        })
+    ];
+
+    console.log(cookies);
+
+    if (cookies.every((cookie) => cookie !== null)) {
+        document.getElementById(
+            "description"
+        ).innerHTML = `Existing Auth Cookies found: <br>
+            ${cookies
                 .map(
                     (cookie) =>
-                        cookie.name +
-                        ": " +
-                        cookie.value.substring(0, 5) +
-                        "..."
+                        `${cookie.name}: ${cookie.value.substring(0, 5)}...`
                 )
-                .join("<br>");
+                .join("<br>")}`;
         document.getElementById("loginButton").innerText = "Refresh Session";
     } else {
         document.getElementById("description").innerText =
             "No Cookies found, please login to Focus to authenticate.";
         document.getElementById("loginButton").innerText = "Log In";
     }
-});
+})();
